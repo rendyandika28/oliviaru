ace969fd46169e4817b7070ef490ee89152c389d
rendy.andika <rendy.andika@privyid.tech>
2025-03-10 15:52:11 +0700

    installing persona

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/tailwind.css'],
+  modules: [
+    '@privyid/persona'
+  ],
+  postcss: {
+    plugins: {
+      'postcss-hexrgba': {},
+      'tailwindcss/nesting': {},
+      'tailwindcss': {},
+      'postcss-lighten-darken': {},
+      'autoprefixer': {},
+    },
+  },
 })
